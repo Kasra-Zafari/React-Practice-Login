9f5c401848f2da9ecc32da86ab5c64609fa51810
Kasra Zafari <zafari.kasra@gmail.com>
2024-12-29 22:59:40 +0330

    phoneNumber componet

--- src/componets/Header/index.jsx
+++ src/componets/Header/index.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import classes from "./index.module.css"
 
 
--- src/componets/PhoneNumber/index.jsx
+++ src/componets/PhoneNumber/index.jsx
@@ -1,5 +1,19 @@
-const PhoneNumber= ()=>{
-return <h3>mobile</h3>
+import classes from "./index.module.css"
+
+
+const PhoneNumber = () => {
+    return (
+        <>
+            <div className={classes.phoneNumber}>
+                <h1>LOGIN</h1>
+                <form className={classes.form}>
+                    <h3>Please Enter Phone Number</h3>
+                    <input type="text" placeholder="phone number" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </>
+    )
 }
 
 
