dd04df73c0cee1fd1cd820022f099ad2a77c46b9
Kasra Zafari <zafari.kasra@gmail.com>
2024-12-29 21:11:28 +0330

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './componets/Header'
+import Home from './componets/Home'
 // import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Home/>
     </>
   )
 }
---
+++ src/componets/Home/index.jsx
@@ -0,0 +1,9 @@
+import classes from "./index.module.css"
+
+const Home = ()=>{
+
+    return <h1 className={classes.home}>Welcome to my site</h1>
+
+}
+
+export default Home;
