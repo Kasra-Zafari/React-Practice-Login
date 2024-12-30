aa0665f05ef48d8d3ad77a61484b6c84e4621735
Kasra Zafari <zafari.kasra@gmail.com>
2024-12-30 20:11:04 +0330

    pattern number

--- src/componets/PhoneNumber/index.jsx
+++ src/componets/PhoneNumber/index.jsx
@@ -1,14 +1,34 @@
 import { useRef } from "react";
 import classes from "./index.module.css"
+import { useState } from "react";
 
 
 const PhoneNumber = () => {
-    const inputRef = useRef(null);
+    const [phoneNumber, setPhoneNumber]=useState("");
+    const [hasError, setHasError] = useState(false);
 
-    const handlePhoneNumberSubmit = (event)=>{
-        event.preventDefault()
+    const inputPattern = /^09[0-9]{9}$/;
+
+    const handlePhoneNumberSubmit = (event) => {
+        event.preventDefault();
         console.log(inputRef.current.value);
-    }
+    };
+
+
+    const handlePhoneNumberChange = (event) => {
+        const inputValue = event.target.value;
+        setPhoneNumber(inputValue);
+
+        if (inputPattern.test(inputValue)) {
+            setHasError(false);
+        }
+
+        else {
+            setHasError(true);
+
+        }
+
+    };
 
     return (
         <>
@@ -17,13 +37,23 @@ const PhoneNumber = () => {
                 <p>Click on the LOGO to return to the home page</p>
                 <form className={classes.form}>
                     <h3>Please Enter Phone Number</h3>
-                    <input type="text" placeholder="phone number" ref={inputRef}/>
+
+                    <input
+                        type="text"
+                        placeholder="phone number"
+                        onChange={handlePhoneNumberChange}
+                        value={phoneNumber}
+                        className={hasError && classes.error}
+                    />
+
+                    {hasError && <p>Please enter the correct mobile number</p>}
+
                     <button type="submit" onClick={handlePhoneNumberSubmit}>Submit</button>
                 </form>
             </div>
         </>
-    )
-}
+    );
+};
 
 
 export default PhoneNumber;
