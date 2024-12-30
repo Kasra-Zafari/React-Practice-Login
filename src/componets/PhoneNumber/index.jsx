353d33670572e899c48cbc2b62da38cc69987dd8
Kasra Zafari <zafari.kasra@gmail.com>
2024-12-30 19:13:44 +0330

    handlePhoneNumberSubmi

--- src/componets/PhoneNumber/index.jsx
+++ src/componets/PhoneNumber/index.jsx
@@ -1,16 +1,24 @@
+import { useRef } from "react";
 import classes from "./index.module.css"
 
 
 const PhoneNumber = () => {
+    const inputRef = useRef(null);
+
+    const handlePhoneNumberSubmit = (event)=>{
+        event.preventDefault()
+        console.log(inputRef.current.value);
+    }
+
     return (
         <>
             <div className={classes.phoneNumber}>
                 <h1>LOGIN</h1>
-                <p>Click on the logo to return to the home page</p>
+                <p>Click on the LOGO to return to the home page</p>
                 <form className={classes.form}>
                     <h3>Please Enter Phone Number</h3>
-                    <input type="text" placeholder="phone number" />
-                    <button type="submit">Submit</button>
+                    <input type="text" placeholder="phone number" ref={inputRef}/>
+                    <button type="submit" onClick={handlePhoneNumberSubmit}>Submit</button>
                 </form>
             </div>
         </>
