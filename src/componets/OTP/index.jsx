3706cbcf3845bba207d072d88d683fde3b59d5f5
Kasra Zafari <zafari.kasra@gmail.com>
2024-12-31 22:05:01 +0330

    checked otp code

--- src/componets/OTP/index.jsx
+++ src/componets/OTP/index.jsx
@@ -1,9 +1,21 @@
 import classes from "./index.module.css"
+import { useRef } from "react";
 
 const Otp = (props)=>{
 
+    const inputRef = useRef(null);
+
     const handleOtpSubmit = (event)=>{
         event.preventDefault();
+        const check = Number(inputRef.current.value)===props.otpCode;
+        if(check){
+            console.log("ok");
+            
+        }
+        else{
+            console.log("not ok");
+            
+        }
             
     }
 
@@ -18,7 +30,7 @@ const Otp = (props)=>{
         <form className={classes.form}>
         <h3>Please Enter OTP</h3>
         <p>Enter the OTP code sent to the phone number {props.phoneNumber} </p>
-        <input type="text" className={classes.input}/>
+        <input type="text" className={classes.input} ref={inputRef}/>
         <button type="submit" className={classes.button} onClick={handleOtpSubmit}>SUBMIT</button>
         </form>
         </div>
