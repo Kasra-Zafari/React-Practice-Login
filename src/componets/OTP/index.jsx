517b4534c8554a3c5050b7de20da6f9350aa7f8c
Kasra Zafari <zafari.kasra@gmail.com>
2024-12-31 22:19:18 +0330

    empty input / disabled submit

--- src/componets/OTP/index.jsx
+++ src/componets/OTP/index.jsx
@@ -1,39 +1,55 @@
 import classes from "./index.module.css"
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-const Otp = (props)=>{
+const Otp = (props) => {
 
     const inputRef = useRef(null);
+    const [otpInput, setOtpInput] = useState("")
 
-    const handleOtpSubmit = (event)=>{
+    const handleInputChange = (event) => {
+        setOtpInput(event.target.value);
+    }
+
+    const handleOtpSubmit = (event) => {
         event.preventDefault();
-        const check = Number(inputRef.current.value)===props.otpCode;
-        if(check){
+        const check = Number(inputRef.current.value) === props.otpCode;
+        if (check) {
             console.log("ok");
-            
+
         }
-        else{
+        else {
             console.log("not ok");
-            
+
         }
-            
+
     }
 
 
     return (
         <>
-        <div className={classes.otp}>
-        <h1>OTP</h1>
-        <p>Click on the LOGO to return to the home page</p>
-        <p>Click on the LOGIN to return to the login page</p>
+            <div className={classes.otp}>
+                <h1>OTP</h1>
+                <p>Click on the LOGO to return to the home page</p>
+                <p>Click on the LOGIN to return to the login page</p>
 
-        <form className={classes.form}>
-        <h3>Please Enter OTP</h3>
-        <p>Enter the OTP code sent to the phone number {props.phoneNumber} </p>
-        <input type="text" className={classes.input} ref={inputRef}/>
-        <button type="submit" className={classes.button} onClick={handleOtpSubmit}>SUBMIT</button>
-        </form>
-        </div>
+                <form className={classes.form}>
+                    <h3>Please Enter OTP</h3>
+                    <p>Enter the OTP code sent to the phone number {props.phoneNumber} </p>
+                    <input
+                    type="text"
+                    className={classes.input}
+                    ref={inputRef}
+                    onChange={handleInputChange}
+                    value={otpInput} />
+
+                    <button
+                    type="submit"
+                    className={classes.button}
+                    onClick={handleOtpSubmit}
+                    disabled={otpInput===""}>
+                        SUBMIT</button>
+                </form>
+            </div>
         </>
     )
 }
